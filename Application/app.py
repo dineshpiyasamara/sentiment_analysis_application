dabb0b82e300a4b03b14d103c00e53e4d749c60a
dinesh <silvershine.zero@gmail.com>
2022-12-21 11:20:10 +0530

    update

--- Application/app.py
+++ Application/app.py
@@ -8,22 +8,27 @@ from nltk.stem import WordNetLemmatizer
 
 lemmatizer = WordNetLemmatizer()
 sw = stopwords.words("english")
-with open('static/model/model_pkl' , 'rb') as f:
+with open('static/model/model_pkl', 'rb') as f:
     model = pickle.load(f)
 vocab = pd.read_csv('static/model/vocabulary.txt', header=None)
 tokens = vocab[0].tolist()
-    
+
+
 def preprocessing(text):
     data = pd.DataFrame([text])
     data[0] = data[0].apply(lambda x: " ".join(x.lower() for x in x.split()))
-    data[0] = data[0].apply(lambda x: " ".join(re.sub(r'^https?:\/\/.*[\r\n]*', '', x, flags=re.MULTILINE) for x in x.split()))
-    data[0] = data[0].str.replace('[^\w\s]','')
-    data[0] = data[0].str.replace('\d','')
-    data[0] = data[0].apply(lambda x: " ".join(x for x in x.split() if x not in sw))
-    data[0] = data[0].apply(lambda x: " ".join([lemmatizer.lemmatize(word) for word in x.split()]))
+    data[0] = data[0].apply(lambda x: " ".join(
+        re.sub(r'^https?:\/\/.*[\r\n]*', '', x, flags=re.MULTILINE) for x in x.split()))
+    data[0] = data[0].str.replace('[^\w\s]', '')
+    data[0] = data[0].str.replace('\d', '')
+    data[0] = data[0].apply(lambda x: " ".join(
+        x for x in x.split() if x not in sw))
+    data[0] = data[0].apply(lambda x: " ".join(
+        [lemmatizer.lemmatize(word) for word in x.split()]))
     data = data[0]
     pred = get_pred(data)
     return pred
+
 
 def get_pred(data):
     vectorized_test_lst = []
@@ -36,12 +41,14 @@ def get_pred(data):
     tested = np.asarray(vectorized_test_lst, dtype=np.float32)
     return model.predict(tested)
 
+
 app = Flask(__name__)
 
 data = dict()
 reviews = []
 negative = 0
 positive = 0
+
 
 @app.route('/')
 def index():
@@ -50,18 +57,20 @@ def index():
     data['positive'] = positive
     return render_template('index.html', data=data)
 
+
 @app.route('/', methods=['POST'])
 def my_form_post():
     text = request.form['text']
     pred = preprocessing(text)
     if pred == 1:
         global negative
-        negative+=1
+        negative += 1
     else:
         global positive
-        positive+=1
+        positive += 1
     reviews.insert(0, text)
     return redirect(request.url)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
